08a819ec5daf540478b7d1e1beef6252d3699f35
Resident234Photo <gsu4306@gmail.com>
2025-09-06 19:08:48 +0300

    **Add banner helper and main entry point** Introduced `banner.py` to display an ASCII banner via the `show_banner` function. Added `main.py` as the script's entry point referencing `checkpoint.main()`.

---
+++ checkpoint/helpers/banner.py
@@ -0,0 +1,25 @@
+from checkpoint import globals as gb
+
+def show_banner():
+
+    banner = """
+    [red] .o88b. [/][blue]db   db[/][red] .88b  d88. [/][green]88888b. 
+    [/][red]d8P  Y8 [/][blue]88   88[/][red] 88'YbdP`88 [/][green]88  `8D 
+    [/][red]8P      [/][blue]88ooo88[/][red] 88  88  88 [/][green]88   88 
+    [/][red]8b      [/][blue]88~~~88[/][red] 88  88  88 [/][green]88   88 
+    [/][red]Y8b  d8 [/][blue]88   88[/][red] 88  88  88 [/][green]88  .8D 
+    [/][red] `Y88P' [/][blue]YP   YP[/][red] YP  YP  YP [/][green]Y8888D'[/] v2
+
+    [yellow] ██████[/][red] ██   ██[/][yellow] ███████[/][blue] ██████[/][green] ██   ██
+    [/][yellow]██      [/][red]██   ██[/][yellow] ██      [/][blue]██      [/][green]██  ██ 
+    [/][yellow]██      [/][red]███████[/][yellow] █████   [/][blue]██      [/][green]█████  
+    [/][yellow]██      [/][red]██   ██[/][yellow] ██      [/][blue]██      [/][green]██  ██ 
+    [/][yellow] ██████[/][red] ██   ██[/][yellow] ███████[/][blue] ██████[/][green] ██   ██
+
+    [magenta]            Facebook Management Tool
+
+             [bold]By: Resident234 (🐦 [deep_sky_blue1][link=https://x.com/Resident234]@Resident234[/link][/deep_sky_blue1])
+       
+    """
+    # [indian_red1]Support my work on GitHub Sponsors ! 💖[/indian_red1][/bold]
+    gb.rc.print(banner)
